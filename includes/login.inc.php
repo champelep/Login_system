0cbf41bd2a8f1b475855443141ff8162fa2dd66d
elep <champ.supakit@gmail.com>
2019-08-10 12:39:27 +0700

    inprocess-login script

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,42 +1,21 @@
 <?php
-if (isset($_POST["login-submit"])) {
-	
-	require "dbh.inc.php";
 
-	$username = $_POST["mailuid"];
-	// $email = $_POST["mailuid"];
-	$password = $_POST["pwd"];
+if(isset($_POST['login-submit'])) {
 
-	if (empty($username) || empty($email) || empty($password) ) {
-		header("location: ../signup.php?error=emptyfields&uid=".$username."&mail".$email);
+	require(dbh.inc.php);
+
+	$mailuid = $_POST['mailuid'];
+	$password = $_POST['pwd'];
+
+	if(empty($mailuid) || empty($password)){
+		header("location: ../index.php?error=emptymailuidorpassword");
 		exit();
 	}
-	elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-		header("location: ../signup.php?error=invalidmailuid");
-		exit();
+	else {
+		$sql = "SELECT *  FROM users WHERE uidUsers=? || emailUsers=?";
 	}
-	elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-		header("location: ../signup.php?error=invalidmail&uid=".$username);
-		exit();
-	}
-	elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
-		header("location: ../signup.php?error=invladinuid&mail=".$mail);
-		exit();
-	}
-
-	$sql = "SELECT * FROM users 
-	WHERE (username = '$username' OR email = '$username')
-	AND password = '$password' ";
-
-	if ($conn->query($sql) === TRUE) { 
-		echo "login sucessfully";
-	} else {
-		echo "Error: " .$sql. "<br>". $conn->error;
-		}
-$conn->close();
-	}
-
-
-
-
-?>
+}
+else {
+	header("location: ../index.php");
+	exit();
+}
