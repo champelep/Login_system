c791b47d4a5afa65f89c78edeb63bb24b72b2492
elep <champ.supakit@gmail.com>
2019-08-10 12:49:49 +0700

    test commit

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -13,6 +13,15 @@ if(isset($_POST['login-submit'])) {
 	}
 	else {
 		$sql = "SELECT *  FROM users WHERE uidUsers=? || emailUsers=?";
+		$stmt = mysqli_stmt_init($conn);
+		if(!mysqli_stmt_prepare($stmt, $sql)) {
+			header("location: ../index.php?error=sqlerror");
+			exit();		
+		}
+		else {
+			mysqli_stmt_bind_param($stmt,"ss", $mailuid, $password);
+			mysqli_stmt_execute($stmt);
+		}
 	}
 }
 else {
