14bf7ef278b2816f19155ed8b12feebce8ad67a8
elep <champ.supakit@gmail.com>
2019-08-15 02:48:23 +0700

    now it all works

--- includes/portfolio.inc.php
+++ includes/portfolio.inc.php
@@ -1,7 +1,21 @@
 <?php
+session_start();
+// var_dump($_POST);
+// var_dump($_SESSION['userId']);
+// die();
+//mysqli connectivity, select database
+$connect= new mysqli("localhost","root","","loginsystem") or die("ERROR:could not connect to the database!!!");
 
-if (isset($_POST["save-message"])) {
-	
-	require "dbh.inc.php";
+//extract all html field
+// extract($_POST);
+if( isset($_POST['save']) && $_POST['save'] ){
+	//store textarea values in <pre> tag
+	$msg = $_POST["savedMessage"];
+	//insert values in textarea table
+	$query="UPDATE users SET textarea = '$msg' WHERE idUsers = ". $_SESSION['userId'];
+	$connect->query($query);
+	echo "Data saved";
 
-	$message = $_POST["message"];
+}
+
+?>
--- portfolio.php
+++ portfolio.php
@@ -3,8 +3,25 @@
 session_start();
 	if(!(isset($_SESSION['userId']) && $_SESSION['userId'] != '')){
 		header ("Location:index.php?error=notloggedin");
-	
 	}
+
+	// = code starts here
+	if(isset($_SESSION['userId']) && $_SESSION['userId']){
+		$con= new mysqli("localhost","root","","loginsystem") or die("ERROR:could not connect to the database!!!");
+		$sql="SELECT * FROM users WHERE idUsers = ". $_SESSION['userId'];
+	}
+	if ($result=mysqli_query($con,$sql))
+  {
+  // Fetch one and one row
+  	while ($row = mysqli_fetch_assoc($result))
+    {
+    	// var_dump($row);
+    	// echo "<br>";
+   		$msg = $row['textarea'];
+    }
+  // Free result set
+ 		 mysqli_free_result($result);
+}
 ?> 
 <html>
 
@@ -15,10 +32,11 @@ session_start();
 <a href="#"> 
 	<img src="img/logo.png" alt="logo" width="100" height="100">
 	<br>
-	<form>
-		<form action="includes/portfolio.inc.php" method="post">
-		<textarea name = "message" rows = "30" cols = "50" placeholder="enter your mesage"></textarea><br>
-		<button type = "submit" name = "save-message"> Save Message </button>
+	<form action="includes/portfolio.inc.php" method="post">
+		<textarea name = "savedMessage" rows = "30" cols = "50" placeholder="enter your mesage"><?php echo $msg; ?></textarea>
+		<br>
+		<input type = "submit" value = "Save" name = "save">
+		<input type="submit" value = "Display" name="disp"/>
 	</form>
 </body>
 </html>
