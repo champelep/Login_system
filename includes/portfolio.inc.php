1017c9596548ee98d21e6e68248337fdb01af2b5
elep <champ.supakit@gmail.com>
2019-08-22 22:01:37 +0700

    .

--- includes/portfolio.inc.php
+++ includes/portfolio.inc.php
@@ -24,6 +24,7 @@ $target_dir = "Upload/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+//$newfilename = round(microtime(true)) . '.' . end($temp);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
